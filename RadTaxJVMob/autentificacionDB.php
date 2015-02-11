94ebe34c192c62b685984821c0b010a5c8715c91
diplez <diegopl_l@hotmail.com>
2015-02-10 22:30:19 -0500

    v.1

--- RadTaxJVMob/autentificacionDB.php
+++ RadTaxJVMob/autentificacionDB.php
@@ -10,7 +10,7 @@ header('Content-type: text/html; charset=iso-8859-1');
 header('Content-Type: text/html; charset=UTF-8');
 header('Cache-Control: no-store, no-cache, must-revalidate'); //para que no controle la cache.control de seguridad.
 
-$conexion = mysqli_connect("127.13.165.2:3306", "adminEHXtNmV", "3QPx-SZY4u1_", "taxicall") or die("Error " . mysqli_error($link));
+$conexion = mysqli_connect("127.13.165.2", "adminEHXtNmV", "3QPx-SZY4u1_", "taxicall") or die("Error coneccion" . mysqli_error($link));
 
 $usuario = addslashes(htmlspecialchars($_POST['usuario']));
 $clave = addslashes(htmlspecialchars($_POST['clave']));
