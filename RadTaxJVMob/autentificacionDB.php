becf8afebf28188a5dd36a5508173d502cb9edf4
diplez <diegopl_l@hotmail.com>
2015-02-10 22:25:14 -0500

    actual

--- RadTaxJVMob/autentificacionDB.php
+++ RadTaxJVMob/autentificacionDB.php
@@ -5,15 +5,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-session_start(); //actualiza la noticia 
 header('Content-Type:text/plain'); //texto de tipo plano
 header('Content-type: text/html; charset=iso-8859-1');
 header('Content-Type: text/html; charset=UTF-8');
 header('Cache-Control: no-store, no-cache, must-revalidate'); //para que no controle la cache.control de seguridad.
 
-$conexion = mysqli_connect("localhost", "root", "", "radioTaxijvm") or die("Error " . mysqli_error($link));
+$conexion = mysqli_connect("127.13.165.2:3306", "adminEHXtNmV", "3QPx-SZY4u1_", "taxicall") or die("Error " . mysqli_error($link));
 
 $usuario = addslashes(htmlspecialchars($_POST['usuario']));
 $clave = addslashes(htmlspecialchars($_POST['clave']));
